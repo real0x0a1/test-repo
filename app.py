fa61c4507f3f5af718fadd8688f480cfcced0222
real0x0a1 <143189904+real0x0a1@users.noreply.github.com>
2024-06-18 12:20:51 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {'Hello': 'FastAPI'}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('app:app', host='0.0.0.0', port=8000, reload=True)
